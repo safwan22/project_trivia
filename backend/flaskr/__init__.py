2b26977480e7637a31847f6ff3a7a4e0b36cecc3
Safwan Hussaini <safwanhussaini70@gmail.com>
2022-07-04 00:30:13 +0000

    second

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -55,9 +55,225 @@ def create_app(test_config=None):
             })
         except Exception:
             abort(422)
-
+            
     @app.route('/questions', methods=['GET'])
     def retrieve_questions():
+
+        try:
+            page = request.args.get('page', 1, type=int)
+
+            questions = Question.query \
+                .order_by(Question.id) \
+                .paginate(page=page, per_page=QUESTIONS_PER_PAGE)
+
+            questions_formatted = [
+              question.format() for question in questions.items
+            ]
+
+            # categories
+            categories = Category.query.order_by(Category.id).all()
+            categories_formatted = {
+              category.id: category.type for category in categories
+            }
+
+            if len(questions_formatted) == 0:
+                abort(404)
+            else:
+                return jsonify({
+                  'success': True,
+                  'questions': questions_formatted,
+                  'total_questions': questions.total,
+                  'categories': categories_formatted,
+                  'current_category': None,
+                })
+        except Exception as e:
+            if '404' in str(e):
+                abort(404)
+            else:
+                abort(422)
+
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query \
+              .filter(Question.id == question_id) \
+              .one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify({
+              'success': True,
+              'deleted': question_id,
+            })
+        except Exception as e:
+            if '404' in str(e):
+                abort(404)
+            else:
+                abort(422)
+
+    @app.route('/questions', methods=['POST']) 
+    def create_question():
+        body = request.get_json()
+        question = body.get('question', None)
+        answer = body.get('answer', None)
+        difficulty = body.get('difficulty', None)
+        category = body.get('category', None)
+
+        try:
+            # Create question
+            question = Question(
+              question=question,
+              answer=answer,
+              difficulty=difficulty,
+              category=category
+            )
+
+
+            question.insert()
+
+            return jsonify({
+              'success': True,
+              'created': question.id,
+            })
+        except Exception:
+            abort(422)
+
+    @app.route('/search', methods=['POST'])
+    def search_question():
+        body = request.get_json()
+        search = body.get('searchTerm', None)
+
+        try:
+            questions = Question.query \
+              .order_by(Question.id) \
+              .filter(Question.question.ilike('%{}%'.format(search)))
+
+            questions_formatted = [
+              question.format() for question in questions
+            ]
+
+            return jsonify({
+              'success': True,
+              'questions': questions_formatted,
+              'total_questions': len(questions.all()),
+              'current_category': None,
+            })
+        except Exception:
+            abort(422)
+
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def retrieve_questions_by_category(category_id):
+
+        try:
+    
+            page = request.args.get('page', 1, type=int)
+
+            questions = Question.query \
+                .order_by(Question.id) \
+                .filter(Question.category == category_id) \
+                .paginate(page=page, per_page=QUESTIONS_PER_PAGE)
+
+            questions_formatted = [
+              question.format() for question in questions.items
+            ]
+
+            if len(questions_formatted) == 0:
+                abort(404)
+            else:
+                return jsonify({
+                  'success': True,
+                  'questions': questions_formatted,
+                  'total_questions': questions.total,
+                  'current_category': category_id
+                })
+        except Exception as e:
+            if '404' in str(e):
+                abort(404)
+            else:
+                abort(422)
+
+    @app.route('/quizzes', methods=['POST'])
+    def retrieve_quizzes():
+      
+        try:
+           
+            questions = None
+            body = request.get_json()
+            quiz_category = body.get('quiz_category', None)
+            previous_ids = body.get('previous_questions', None)
+            category_id = quiz_category.get('id')
+
+
+            if category_id == CATEGORY_ALL:
+               
+                questions = Question.query.all()
+            else:
+              
+                questions = Question.query \
+                    .filter(Question.category == category_id) \
+                    .all()
+
+            
+            ids = get_ids_from_questions(questions, previous_ids)
+
+            if len(ids) == 0:
+        
+                return jsonify({
+                  'success': True,
+                  'question': None
+                })
+            else:
+            
+                random_id = random.choice(ids)
+
+                question = Question.query.get(random_id)
+
+                return jsonify({
+                  'success': True,
+                  'question': question.format()
+                })
+
+        except Exception:
+            abort(422)
+
+  
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        print(error)
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        print(error)
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 500
+
     """
     @TODO:
     Create an endpoint to handle GET requests
@@ -71,7 +287,8 @@ def create_app(test_config=None):
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
     number of total questions, current category, categories.
-
+    
+   
     TEST: At this point, when you start the application
     you should see questions and categories generated,
     ten questions per page and pagination at the bottom of the screen for three pages.
